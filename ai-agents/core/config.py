fc8b3e8e7820ee9b8f90f759088cea674b962a08
Dhanumalyan28 <adhanumalyan@gmail.com>
2026-08-11 18:44:39 +0530

    summa

---
+++ ai-agents/core/config.py
@@ -0,0 +1,3 @@
+"""na intha file oru change panni git la push pannuren yen branch la push pannurenn"""
+print("na intha file oru change panni git la push pannuren yen branch la push pannurenn")
+"""save pannu"""
